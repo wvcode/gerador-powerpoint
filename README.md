bc4795d6d70a783fcfb9a1bac69637ed797fe8dd
Walter Ritzel Paixao Cortes <wpcortes@gmail.com>
2023-06-07 19:22:32 -0300

    feat: primeira versão do script

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# gerador-powerpoint
-Estudo de uso da api do openai para gerar apresentações powerpoint a partir de documentos PDF.
+# Gerador de Powerpoint
+
+Este script Python, chamado `main.py`, usa várias bibliotecas para criar uma apresentação de slides Powerpoint (.pptx) a partir de documentos PDF. Ele faz isso utilizando o modelo de linguagem GPT da OpenAI e a API de imagens da OpenAI para gerar imagens.
+
+O script lê documentos PDF e, usando o modelo GPT, transforma esse conteúdo em um conjunto de slides no formato JSON. Em seguida, ele cria um arquivo de apresentação Powerpoint (.pptx) para cada documento, com cada slide contendo um título, conteúdo e uma imagem conceitual gerada a partir da API de imagens da OpenAI.
+
+## Pacotes Necessários
+Os pacotes Python necessários para executar este script são:
+- pandas
+- pyarrow
+- llama-index
+- gpt-index
+- openai
+- pypdf
+- python-dotenv
+- typer
+- python-pptx
+- requests
+
+## Variáveis de Ambiente
+O script requer a seguinte variável de ambiente:
+
+- `OPENAI_API_KEY`: A chave de API para o serviço OpenAI. Esta chave é necessária para acessar os modelos de linguagem e a API de imagens da OpenAI.
+
+## Configurando o Ambiente
+Para executar este script Python, siga as seguintes etapas:
+
+1. Instale os pacotes necessários usando o arquivo `requirements.txt`:
+
+```bash
+pip install -r requirements.txt
+```
+
+2. Configure a variável de ambiente. Crie um arquivo `.env` no diretório do script e adicione sua chave de API para a OpenAI:
+
+```bash
+echo "OPENAI_API_KEY=<sua chave da OpenAI>" >> .env
+```
+
+Lembre-se de substituir `<sua chave da OpenAI>` pela sua chave real.
+
+3. Execute o script com os parâmetros necessários:
+
+```bash
+python main.py <diretorio_de_pdfs> --output_folder <diretorio_de_saida> --index_folder <diretorio_temporario>
+```
+
+Substitua `<diretorio_de_pdfs>`, `<diretorio_de_saida>` e `<diretorio_temporario>` pelos seus diretórios reais.
+
+## Notas
+
+- As pastas de saída e temporária serão criadas automaticamente pelo script se elas não existirem.
+- Se o diretório temporário já existir, todos os arquivos nele serão apagados antes de começar a processar os novos documentos.
+- Todos os documentos PDF no diretório de pdfs serão processados.
+- A chave da OpenAI é necessária para a execução do script, não esqueça de configurar a variável de ambiente corretamente.
---
+++ main.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+from io import BytesIO
+import json
+from llama_index import (
+    GPTVectorStoreIndex,
+    SimpleDirectoryReader,
+    LLMPredictor,
+    PromptHelper,
+    ServiceContext,
+)
+from langchain import OpenAI
+from pptx import Presentation
+from pptx.util import Inches
+from dotenv import load_dotenv
+
+import os
+import shutil
+import requests
+import typer
+import openai
+
+app = typer.Typer()
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def create_query_engine(source_folder):
+    # Carrega lista de documentos do folder que será indexado
+    documents = SimpleDirectoryReader(source_folder).load_data()
+
+    # Define parametros para o GPT
+    max_input_size = 4096
+    num_output = 2048
+    max_chunk_overlap = 20
+
+    # Cria o preditor, o prompt helper e o contexto de serviço que permitirá a indexação do conteúdo
+    llm_predictor = LLMPredictor(
+        llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=num_output)
+    )
+    prompt_helper = PromptHelper(
+        max_input_size=max_input_size,
+        num_output=num_output,
+        max_chunk_overlap=max_chunk_overlap,
+    )
+    service_context = ServiceContext.from_defaults(
+        llm_predictor=llm_predictor, prompt_helper=prompt_helper
+    )
+
+    # Cria o índice baseado nos documentos, utilizando o contexto de serviço
+    index = GPTVectorStoreIndex.from_documents(
+        documents, service_context=service_context
+    )
+
+    # Torna o índice disponível para a 'conversa'
+    qe = index.as_query_engine()
+    return qe
+
+
+def download_image_unsplash(keyword):
+    url = f"https://api.unsplash.com/photos/random?query={keyword}&client_id={os.getenv('UNSPLASH_ACCESS_KEY')}"
+    response = requests.get(url)
+    data = response.json()
+    image_url = data["urls"]["regular"]
+    image_response = requests.get(image_url)
+    image_data = BytesIO(image_response.content)
+    return image_data
+
+
+def download_image(description):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    response_data = openai.Image.create(prompt=description, n=2, size="1024x1024")
+    image_url = response_data["data"][0]["url"].strip()
+    image_response = requests.get(image_url)
+    image_data = BytesIO(image_response.content)
+    return image_data
+
+
+def create_slide(prs, title, content, image_concept):
+    slide_layout = prs.slide_layouts[1]  # Usando o layout de título e conteúdo
+    slide = prs.slides.add_slide(slide_layout)
+
+    # Adicionando título
+    title_placeholder = slide.shapes.title
+    title_placeholder.text = title
+
+    # Adicionando conteúdo
+    content_placeholder = slide.placeholders[1]
+    content_placeholder.text = content
+
+    # Adicionando imagem conceito
+    image_data = download_image(image_concept)
+    image = slide.shapes.add_picture(image_data, Inches(1), Inches(1), height=Inches(3))
+
+    return slide
+
+
+def create_presentation(source_filename, presentation_filename):
+    # Criando uma apresentação vazia
+    prs = Presentation()
+
+    slides = json.load(open(source_filename, "r"))
+
+    for slide in slides:
+        title1 = slide["title"]
+        content1 = slide["content"]
+        image_concept1 = slide["image"]
+        create_slide(prs, title1, content1, image_concept1)
+
+    # Salvar a apresentação em um arquivo
+    prs.save(presentation_filename)
+
+
+@app.command()
+def generate_script_pptx(
+    source_folder: str, output_folder: str = "outputs/pptx", index_folder: str = "temp"
+):
+    files = []
+    for root, _, filenames in os.walk(source_folder):
+        for filename in filenames:
+            if os.path.splitext(filename)[1] == ".pdf":
+                files.append(os.path.join(root, filename))
+
+    if len(files) > 0:
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        for item in files:
+            print(item)
+
+            if not os.path.exists(index_folder):
+                os.makedirs(index_folder)
+            else:
+                for del_filename in os.listdir(index_folder):
+                    os.remove(os.path.join(index_folder, del_filename))
+
+            new_filename = os.path.split(item)[1]
+            shutil.copy2(item, os.path.join(index_folder, new_filename))
+
+            qe = create_query_engine(index_folder)
+
+            comando = """Transforme o conteúdo deste documento em um conjunto de 10 slides
+            formatados em um array de objetos json. Cada slide deve ser um elemento do array, com os seguintes campos: 
+            title que é o titulo do slide, content que conterá o conteúdo do slide e image, que conterá uma expressão que sumarize o conteúdo do slide, no máximo em 5 palavras.
+            O texto deve ser reescrito utilizando um tom técnico."""
+
+            response = qe.query(comando)
+
+            with open(
+                os.path.join(output_folder, new_filename.replace(".pdf", ".json")),
+                "w",
+                encoding="utf8",
+            ) as fw:
+                fw.write(str(response))
+
+            create_presentation(
+                os.path.join(output_folder, new_filename.replace(".pdf", ".json")),
+                os.path.join(output_folder, new_filename.replace(".pdf", ".pptx")),
+            )
+
+
+if __name__ == "__main__":
+    app()
